813f75839342941f9e4d091fae8ed9c097f9ef99
shreya badigera <shreyabadigera1@gmail.com>
2024-09-26 14:32:13 +0530

    Add files via upload

---
+++ practice program 12.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 26 09:40:03 2024
+
+@author: shrey
+"""
+
+def next_prime(N):
+  num = N + 1
+
+  
+  while True:
+    
+    is_prime = True
+    for i in range(2, int(num**0.5) + 1):
+      if num % i == 0:
+        is_prime = False
+        break
+
+    
+    if is_prime:
+      return num
+
+    
+    num += 1
+
+N = int(input())
+
+result = next_prime(N)
+print(result)
+
+
+ 
