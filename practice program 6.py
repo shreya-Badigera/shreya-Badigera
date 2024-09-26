c3870931a36882891938f4d2617953c56af89bf9
shreya badigera <shreyabadigera1@gmail.com>
2024-09-26 14:28:31 +0530

    Add files via upload

---
+++ practice program 6.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 25 18:52:37 2024
+
+@author: shrey
+"""
+
+n=int(input())
+arr=list(map(int,input().split()))
+d={}
+if n==1:
+    print(arr[0])
+else:
+    for i in arr:
+        if i not in d:
+            d[i]=1
+        else:
+            d[i]+=1
+    x=sorted(d.items(),key=lambda x:x[1],reverse=True)
+    if x[0][1]==x[1][1]:
+        print(-1)
+    else:
+        print(x[0][0])
