83f7f002520646d96b88f473879ae580999716b4
shreya badigera <shreyabadigera1@gmail.com>
2024-09-26 14:35:45 +0530

    Add files via upload

---
+++ practice program 17.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 26 11:11:12 2024
+
+@author: shrey
+"""
+
+def missing_characters_to_pangram(input_string):
+    # Define the full alphabet
+    alphabet = set('abcdefghijklmnopqrstuvwxyz')
+    
+    input_chars = set(input_string.lower())
+    
+    missing_chars = alphabet - input_chars
+    
+    sorted_missing_chars = sorted(missing_chars)
+    
+    return ''.join(sorted_missing_chars)
+
+# Input reading
+input_string = input()
+result = missing_characters_to_pangram(input_string)
+print(result)
